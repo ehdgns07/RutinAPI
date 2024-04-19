0afc9962c0d85ac359d5e21ec9f4762a612bcebf
이동훈 <ehdgns07@douzone.com>
2024-04-19 11:25:04 +0900

    initialize project

---
+++ src/main/java/com/example/rutinhapi/RutinHapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rutinhapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutinHapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RutinHapiApplication.class, args);
+    }
+
+}
